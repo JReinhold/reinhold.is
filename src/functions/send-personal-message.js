43beb17d1e41784cf3ecf3562b09d585d0bd1072
Jeppe Reinhold <jeppereinhold@gmail.com>
2018-10-08 22:50:09 +0200

    initial netlify functions, update deps

---
+++ src/functions/send-personal-message.js
@@ -0,0 +1,10 @@
+exports.handler = async (event, context) => {
+	
+	const message = JSON.parse(event.body).message;
+	//send email
+	//save status of email receipt
+	//return with correct status code or throw error
+
+	//throw new Error("something went wront")
+	return {statusCode: 200, body: 'niiice'};
+};
