3db5930378766bbdd4e5385298fb459129be22fb
Jeppe Reinhold <jeppe@reinhold.is>
2022-06-21 23:05:09 +0200

    fix prettier and types

--- astro.config.ts
+++ astro.config.ts
@@ -2,12 +2,10 @@ import { defineConfig } from "astro/config";
 
 import svelte from "@astrojs/svelte";
 import deno from "@astrojs/deno";
-import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   adapter: deno({ port: 3000 }),
-  // adapter: netlify(),
   integrations: [svelte({ configFile: false })],
   vite: {
     ssr: {
