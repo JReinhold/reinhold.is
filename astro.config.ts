b5915eb3bd9566863b1dc5bb2616d123b15843b8
Jeppe Reinhold <jeppe@reinhold.is>
2022-06-21 22:06:57 +0200

    static icons instead of astro-icon

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from "astro/config";
 
 import svelte from "@astrojs/svelte";
+import deno from "@astrojs/deno";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: deno({ port: 3000 }),
+  // adapter: netlify(),
   integrations: [svelte({ configFile: false })],
   vite: {
     ssr: {
