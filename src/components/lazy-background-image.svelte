64032212bdaa2c4df8ad3238655ee12f797c693d
Jeppe Reinhold <jeppe@reinhold.is>
2022-07-06 23:43:20 +0200

    sveltify loading strategy

--- src/components/lazy-background-image.svelte
+++ src/components/lazy-background-image.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import type { Theme } from "$lib/get-sun-theme";
-
   import { onMount } from "svelte";
 
   export let theme: Theme;
@@ -8,36 +7,36 @@
   const lowResImageSource = `/backgrounds/${theme.sun}-200.webp`;
   const highResImageSource = `/backgrounds/${theme.sun}-2000.webp`;
 
+  let highResLoaded = false;
+
   onMount(() => {
     const highResImage = new Image();
     // this line here triggers the browser to load the image
     highResImage.src = highResImageSource;
-
-    // when it is done loading, replace the existing image with the high res and unblur it
     highResImage.onload = () => {
-      const backgroundImageElement = document.querySelector(
-        "#background-image",
-      ) as HTMLImageElement;
-      backgroundImageElement.src = highResImageSource;
-      backgroundImageElement.classList.remove("blur");
+      highResLoaded = true;
     };
   });
 </script>
 
-<img id="background-image" class="blur" src={lowResImageSource} alt="" />
+<img
+  class:blur={!highResLoaded}
+  src={highResLoaded ? highResImageSource : lowResImageSource}
+  alt=""
+/>
 
 <style>
   img {
-    z-index: -1;
     position: fixed;
     bottom: 0;
     left: 0;
     width: 100%;
     height: 100%;
-    transition: 1s filter ease-out;
+    z-index: -1;
     user-select: none;
     object-fit: cover;
     pointer-events: none;
+    transition: 1s filter ease-out;
   }
 
   .blur {
