9c4c0d72e48a74e17f069c6c3a76d545d5080526
Jeppe Reinhold <jeppe@reinhold.is>
2025-05-29 23:14:30 +0200

    simplify footer

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -1,7 +1,6 @@
 <footer>
   <span>
-    Built by Jeppe with help from <a href="https://jkelstrup.com">Jonas</a>,
-    using <a href="https://kit.svelte.dev" target="_blank">SvelteKit</a>, source on
+    Built with <a href="https://svelte.dev" target="_blank">Svelte</a> and <a href="https://open-props.style" target="_blank">Open Props</a>. Source code is on
     <a href="https://github.com/jreinhold/reinhold.is">GitHub</a>, deployed to
     <a href="https://pages.cloudflare.com">Cloudflare Pages</a>. Backgrounds by
     Apple.
