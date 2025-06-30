7ff1c777c05a85377724274079c90e5abfee5713
Jeppe Reinhold <jeppe@reinhold.is>
2025-06-30 09:05:04 +0200

    fix footer text size

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,39 +1,40 @@
 <script lang="ts">
   import Link from "./atoms/link.svelte";
+  import Section from "./atoms/section.svelte";
 </script>
 
-<footer>
-  <span>
-    Built with <a href="https://svelte.dev" target="_blank">Svelte</a> and
-    <a href="https://open-props.style" target="_blank">Open Props</a>. Source
-    code is on
-    <a href="https://github.com/jreinhold/reinhold.is">GitHub</a>, deployed to
-    <a href="https://pages.cloudflare.com">Cloudflare Pages</a>. Backgrounds by
-    Apple.
-  </span>
-</footer>
+<Section element="footer">
+  <div class="container">
+    <span class="text-paragraph">
+      Built with <Link
+        href="https://svelte.dev"
+        target="_blank"
+        class="footer-link">Svelte</Link
+      > and
+      <Link href="https://open-props.style" target="_blank" class="footer-link"
+        >Open Props</Link
+      >. Source code is on
+      <Link href="https://github.com/jreinhold/reinhold.is" class="footer-link"
+        >GitHub</Link
+      >, deployed to
+      <Link href="https://pages.cloudflare.com" class="footer-link"
+        >Cloudflare Pages</Link
+      >. Backgrounds by Apple.
+    </span>
+  </div>
+</Section>
 
 <style>
-  footer {
+  .container {
     display: flex;
     justify-content: center;
-
-    margin: 0 var(--size-fluid-1);
-    padding: var(--size-fluid-2);
-    gap: var(--size-1);
-
-    border-radius: var(--radius-3);
-
-    font-size: var(--font-size-0);
-
-    @supports (backdrop-filter: blur(30px)) {
-      backdrop-filter: blur(30px);
-      background: hsl(var(--surface-1-hsl) / 60%);
-    }
-
-    /* stronger background color when backdrop cannot be blurred */
-    @supports not (backdrop-filter: blur(30px)) {
-      background: hsl(var(--surface-1-hsl) / 90%);
-    }
+    text-align: center;
+  }
+  .text-paragraph {
+    text-wrap: balance;
+  }
+  .text-paragraph,
+  :global(.footer-link) {
+    font-size: var(--font-size-1);
   }
 </style>
